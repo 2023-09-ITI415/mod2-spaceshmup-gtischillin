cce852df9c05356beffad51ba97b34dc2bfd71a9
David <davidkoh67@gmail.com>
2023-10-19 18:17:21 -0400

    Movement Speed Modifier
    Added a 10% movement speed boost every time the hero eliminates two enemies.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour {
-    static public Hero S; // Singleton
+public class Hero : MonoBehaviour
+{
+    static public Hero S;
 
-    [Header("Set in Inspector")]
-    // These fields control the movement of the ship
-    public float speed = 30;
+    public float baseSpeed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
     public float gameRestartDelay = 2f;
@@ -15,54 +14,45 @@ public class Hero : MonoBehaviour {
     public float projectileSpeed = 40;
     public Weapon[] weapons;
 
-    [Header("Set Dynamically")]
     [SerializeField]
     public float _shieldLevel = 1;
 
-    // This variable holds a reference to the last triggering GameObject
     private GameObject lastTriggerGo = null;
 
-    // Declare a new delegate type WeaponFireDelegate
     public delegate void WeaponFireDelegate();
-    // Create a WeaponFireDelegate field named fireDelegate.
     public WeaponFireDelegate fireDelegate;
 
-	void Start()
+    private int enemiesKilled = 0;
+
+    void Start()
     {
         if (S == null)
         {
-            S = this; // Set the Singleton
+            S = this;
         }
         else
         {
             Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
         }
-        //fireDelegate += TempFire;
 
-        // Reset the weapons to start _Hero with 1 blaster
         ClearWeapons();
         weapons[0].SetType(WeaponType.blaster);
     }
-	
-	// Update is called once per frame
-	void Update()
+
+    void Update()
     {
-        // Pull in information from the Input class
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
 
-        // Change transform.position based on the axes
+        float speedMultiplier = 1 + (Mathf.Floor(enemiesKilled / 2) * 0.10f);
+
         Vector3 pos = transform.position;
-        pos.x += xAxis * speed * Time.deltaTime;
-        pos.y += yAxis * speed * Time.deltaTime;
+        pos.x += xAxis * (baseSpeed * speedMultiplier) * Time.deltaTime;
+        pos.y += yAxis * (baseSpeed * speedMultiplier) * Time.deltaTime;
         transform.position = pos;
 
-        // Rotate the ship to make it feel more dynamic
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
 
-        // Use the fireDelegate to fire Weapons
-        // First, make sure the button is pressed: Axis("Jump")
-        // Then ensure that fireDelegate isn't null to avoid an error
         if (Input.GetAxis("Jump") == 1 && fireDelegate != null)
         {
             fireDelegate();
@@ -73,28 +63,27 @@ public class Hero : MonoBehaviour {
     {
         Transform rootT = other.gameObject.transform.root;
         GameObject go = rootT.gameObject;
-        print("Triggered: " + go.name);
 
-        // Make sure it's not the same triggering go as last time
         if (go == lastTriggerGo)
         {
             return;
         }
         lastTriggerGo = go;
 
-        if(go.tag == "Enemy")
+        if (go.tag == "Enemy")
         {
             shieldLevel--;
             Destroy(go);
+            enemiesKilled++;
+
+            if (enemiesKilled % 10 == 0)
+            {
+                Debug.Log("Increased speed by 5%");
+            }
         }
         else if (go.tag == "PowerUp")
         {
-            // If the shield was triggered by a PowerUp
             AbsorbPowerUp(go);
-        }
-        else
-        {
-            print("Triggered by non-Enemy: " + go.name);
         }
     }
 
@@ -108,18 +97,16 @@ public class Hero : MonoBehaviour {
                 break;
 
             default:
-                if(pu.type == weapons[0].type)
+                if (pu.type == weapons[0].type)
                 {
                     Weapon w = GetEmptyWeaponSlot();
-                    if(w != null)
+                    if (w != null)
                     {
-                        // Set it to pu.type
                         w.SetType(pu.type);
                     }
                 }
                 else
                 {
-                    //If this is a different weapon type
                     ClearWeapons();
                     weapons[0].SetType(pu.type);
                 }
@@ -137,11 +124,10 @@ public class Hero : MonoBehaviour {
         set
         {
             _shieldLevel = Mathf.Min(value, 4);
-            // If the shield is going to be set to less than zero
+
             if (value < 0)
             {
                 Destroy(this.gameObject);
-                // Tell Main.S to restart the game after a delay
                 Main.S.DelayedRestart(gameRestartDelay);
             }
         }
@@ -149,7 +135,7 @@ public class Hero : MonoBehaviour {
 
     Weapon GetEmptyWeaponSlot()
     {
-        for (int i=0; i<weapons.Length; i++)
+        for (int i = 0; i < weapons.Length; i++)
         {
             if (weapons[i].type == WeaponType.none)
             {
